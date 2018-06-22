480853a74c1e003047297a9c0b7eaf7e074d4480
huhainan <huhainan@dianhun.cn>
2018-06-22 14:32:23 +0800

    js 本地化

--- heat.php
+++ heat.php
@@ -6,8 +6,8 @@
     <!-- 引入 echarts.js -->
     <script src="./static/js/echarts.min.js"></script>
     <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="./static/js/jquery.min.js"></script>
+    <script src="./static/js/bootstrap.min.js"></script>
 </head>
 <body>
 <ul id="myTab" class="nav nav-tabs">
